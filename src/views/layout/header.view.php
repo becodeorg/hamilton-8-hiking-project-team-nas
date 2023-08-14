1f96de6e0e4ce9db0089a3b1e5e41ce99a8724c1
Nicolas Mateo Uroz <nicolasmateo1995@gmail.com>
2023-08-14 15:33:31 +0200

    ajout tailwind

---
+++ src/views/layout/header.view.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hiker'slife</title>
+    <script src="http://cdn.tailwindcss.com/?plugins=forms%22%3E</script>
+</head>
+<body>
+<main>
